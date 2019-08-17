35ddee5eb28cd87c483421a7c5435e228bcd81c1
Try <khovct@gmail.com>
2019-08-16 17:06:13 -0700

    installed native-base and created getUserFromAPI method in App.js

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Image, ActivityIndicator } from 'react-native';
+import { Card, CardItem } from 'native-base';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+
+export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+      dataSource: []
+    }
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
